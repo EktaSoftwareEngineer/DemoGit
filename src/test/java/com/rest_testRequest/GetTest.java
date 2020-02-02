27a5e05b7f4ab08ab38715246af65925508dd296
Ekta Kumar <ekta@Ektas-MBP.lan>
2020-02-01 22:37:28 -0800

    edited the script commit

--- src/test/java/com/rest_testRequest/GetTest.java
+++ src/test/java/com/rest_testRequest/GetTest.java
@@ -1,13 +1,38 @@
 package com.rest_testRequest;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class GetTest {
 	
 	@Test 
-	public void TestApp() {
-		System.out.println("the test result");
+
 		
-	}
+		void getwaetherDetails() {
+			// specify base URI
+			RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+			RequestSpecification httprequest = RestAssured.given();
+			// response object need to be created
+			Response response = httprequest.request(Method.GET, "/Hyderabad");
+			// print response in console window
+			String responseBody = response.getBody().asString();
+			System.out.println("Response Body is :" + responseBody);
+			// how to capture the status code?
+			int statusCode = response.getStatusCode();
+			System.out.println(statusCode);
+			Assert.assertEquals(statusCode, 200);
+			// status line
+			String statusline = response.getStatusLine();
+			System.out.println(statusline);
+			// Assert
+			Assert.assertEquals(statusline, "HTTP/1.1 200 OK");
+
+		}
+
 
 }
