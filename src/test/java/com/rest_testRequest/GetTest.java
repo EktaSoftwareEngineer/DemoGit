cc329eeefcc177f47468ee4d196ba89c2185c9cc
Ekta Kumar <ekta@Ektas-MBP.lan>
2020-02-01 22:07:21 -0800

    first commit

---
+++ src/test/java/com/rest_testRequest/GetTest.java
@@ -0,0 +1,13 @@
+package com.rest_testRequest;
+
+import org.testng.annotations.Test;
+
+public class GetTest {
+	
+	@Test 
+	public void TestApp() {
+		System.out.println("the test result");
+		
+	}
+
+}
